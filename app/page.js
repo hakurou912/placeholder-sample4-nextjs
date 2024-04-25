65d84f2285a9dfc57b4400be2f49c26fdbecf2a0
hakurou912 <db.hakurou@gmail.com>
2024-04-25 17:15:39 +0900

    app

--- app/page.js
+++ app/page.js
@@ -1,95 +1,30 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import NextLink from 'next/link';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+/* Post情報取得 */
+async function getPostsData(){  
+  const response = await fetch(`${process.env.NEXT_PUBLIC_JSONPLACEHOLDER_URL}posts/`, { revalidate: 10, cache: 'no-store' })
+  return response.json();
+}
+
+export default async function Page() {
+  const postList = await getPostsData();
+
+  //各ユーザーIDでPost情報を1件ずつに絞っています（id=1~10がuser1のPost、id=11～20がuser2の人のPost...という感じだったので10の倍数でfilterかけています。
+  const dispList = postList.filter(element => element.id = element.id % 10 === 0);
+
+  return(
+    <>
+    <div className="container p-2 bg-success-subtle">
+      <p>app/page.js</p>
+      <ul className="list-group list-group-flush">
+        {dispList.map((element, index) => (
+          <li key={index} className="list-group-item">
+            <div>{element.title}</div>
+            <div><NextLink href={`/users/${element.userId}`}>userId:{element.userId}</NextLink></div>
+          </li>
+        ))}
+      </ul>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    </>
+  )
 }
